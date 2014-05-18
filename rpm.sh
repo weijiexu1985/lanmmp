0f5ade4d98bb55e4d2128f789112484fa59381f8
weijiexu1985 <32094177@qq.com>
2014-05-18 15:54:16 +0800

    Create rpm.sh

---
+++ rpm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
+export PATH
+
+if [ $(id -u) != "0" ]; then
+printf "Error: You must be root to run this script!"
+exit 1
+fi
